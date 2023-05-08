d1c531efdea43b45d227a66d9699c87d11eddb1f
timmystroge <timmystroge75@gmail.com>
2023-05-08 16:11:11 +0100

    Accept Request from Frontend

--- index.js
+++ index.js
@@ -11,11 +11,46 @@ const app = express();
 
 // set middleware
 app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//? CONNECTION TO DATABASE, DATABASE SCHEMA, DATABASE MODEL
+mongoose.connect(process.env.API_URL);
+try {
+  log("Connected!");
+} catch {
+  log("Failed!");
+}
+
+//Create Schema
+const userSchema = new mongoose.Schema({
+  fullname: { type: String },
+  email: { type: String },
+  password: { type: String, min: 6 },
+});
+
+// Create database Model
+const StrogeDevUsers = mongoose.model("StrogeDevUsers", userSchema);
 //
 app.route("/").get((req, res) => {
-  res.json({ msg: "Backend" });
+  res.json({ msg: "Strge-Dev Server API" });
+});
+
+/*  AUTH/REGISTER */
+app.route("/auth/register").post((req, res) => {
+  const { fullname, email, password } = req.body;
+  log(fullname, email, password);
+
+  // create a new user
+  const user = new StrogeDevUsers({
+    fullname: fullname,
+    email: email,
+    password: password,
+  });
+
+  user.save();
+
+  res.json({ msg: "Account Created Successfully!" });
 });
 
 app.listen(process.env.PORT || 3000, () =>
